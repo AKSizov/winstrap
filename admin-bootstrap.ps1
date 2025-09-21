8c54de278936da54bb8679171d5c4198f29132a6
AKSizov <37084670+AKSizov@users.noreply.github.com>
2025-09-21 17:57:23 -0400

    new: use scoop

--- admin-bootstrap.ps1
+++ admin-bootstrap.ps1
@@ -8,22 +8,3 @@ Set-Itemproperty -path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 
 # Restore legacy right click menu
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
-
-# Install choco
-# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-# Install programs
-# choco install -y firefox
-# choco install -y sunshine
-# choco install -y autohotkey
-
-# choco install -y git
-# choco install -y rsync
-
-# choco install -y blender
-# choco install -y freecad
-# choco install -y gimp
-
-# choco install -y steam
-# choco install -y minecraft-launcher
-# choco install -y epicgameslauncher
--- user-bootstrap.ps1
+++ user-bootstrap.ps1
@@ -5,7 +5,8 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/AKSizov/winstrap/refs/
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/AKSizov/winstrap/refs/heads/master/res/disable_lwin.ahk" -OutFile "C:\docs\res\disable_lwin.ahk"
 explorer C:\docs\res
 
-irm get.scoop.sh | powershell
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 scoop bucket add extras
 scoop bucket add games
 scoop bucket add versions
