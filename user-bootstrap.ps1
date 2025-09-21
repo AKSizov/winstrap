27d0ab49eec2f2ea1d39f989618a2b12bfc508c7
AKSizov <37084670+AKSizov@users.noreply.github.com>
2025-09-21 18:04:49 -0400

    debug: this script...

--- user-bootstrap.ps1
+++ user-bootstrap.ps1
@@ -5,8 +5,13 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/AKSizov/winstrap/refs/
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/AKSizov/winstrap/refs/heads/master/res/disable_lwin.ahk" -OutFile "C:\docs\res\disable_lwin.ahk"
 explorer C:\docs\res
 
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+# Install Scoop only if it's not already installed
+if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
+    irm get.scoop.sh | iex
+}
+
 scoop bucket add extras
 scoop bucket add games
 scoop bucket add versions
@@ -14,3 +19,4 @@ scoop bucket add java
 scoop bucket add nonportable
 scoop install extras/firefox extras/ungoogled-chromium extras/discord extras/tailscale extras/spotify extras/vlc main/ffmpeg extras/cpu-z extras/gpu-z extras/rufus extras/autohotkey extras/obs-studio extras/godot extras/blender extras/gimp main/7zip main/syncthing extras/obsidian versions/steam epic-games-launcher versions/python311 java/openjdk21 extras/vscodium main/git-lfs extras/winscp main/curl
 scoop install nonportable/powertoys-np
+pause
