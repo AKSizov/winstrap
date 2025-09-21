eb532f1523f3957533b7520bc1b533aa7f06e253
AKSizov <37084670+AKSizov@users.noreply.github.com>
2025-09-21 17:53:02 -0400

    new: experimental scoop install

--- admin-bootstrap.ps1
+++ admin-bootstrap.ps1
@@ -6,21 +6,24 @@ New-ItemProperty -Path "HKCU:\Control Panel\Mouse" -Name "MouseThreshold2" -Valu
 # Enable file extensions
 Set-Itemproperty -path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideFileExt' -value 0
 
+# Restore legacy right click menu
+reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+
 # Install choco
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Install programs
-choco install -y firefox
-choco install -y sunshine
-choco install -y autohotkey
+# choco install -y firefox
+# choco install -y sunshine
+# choco install -y autohotkey
 
-choco install -y git
-choco install -y rsync
+# choco install -y git
+# choco install -y rsync
 
-choco install -y blender
-choco install -y freecad
-choco install -y gimp
+# choco install -y blender
+# choco install -y freecad
+# choco install -y gimp
 
-choco install -y steam
-choco install -y minecraft-launcher
-choco install -y epicgameslauncher
+# choco install -y steam
+# choco install -y minecraft-launcher
+# choco install -y epicgameslauncher
--- user-bootstrap.ps1
+++ user-bootstrap.ps1
@@ -4,3 +4,12 @@ mkdir C:\docs\res
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/AKSizov/winstrap/refs/heads/master/res/kanistra-studio-17-service-station.jpg" -OutFile "C:\docs\res\kanistra-studio-17-service-station.jpg"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/AKSizov/winstrap/refs/heads/master/res/disable_lwin.ahk" -OutFile "C:\docs\res\disable_lwin.ahk"
 explorer C:\docs\res
+
+irm get.scoop.sh | powershell
+scoop bucket add extras
+scoop bucket add games
+scoop bucket add versions
+scoop bucket add java
+scoop bucket add nonportable
+scoop install extras/firefox extras/ungoogled-chromium extras/discord extras/tailscale extras/spotify extras/vlc main/ffmpeg extras/cpu-z extras/gpu-z extras/rufus extras/autohotkey extras/obs-studio extras/godot extras/blender extras/gimp main/7zip main/syncthing extras/obsidian versions/steam epic-games-launcher versions/python311 java/openjdk21 extras/vscodium main/git-lfs extras/winscp main/curl
+scoop install nonportable/powertoys-np
