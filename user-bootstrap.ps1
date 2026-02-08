5bccbcb41eeb765a79dcab9f0150db6b9b32e8e3
AKSizov <37084670+AKSizov@users.noreply.github.com>
2026-02-08 01:02:56 -0500

    remove: remove ahk

--- user-bootstrap.ps1
+++ user-bootstrap.ps1
@@ -2,5 +2,5 @@
 mkdir C:\docs
 mkdir C:\docs\res
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/AKSizov/winstrap/refs/heads/master/res/kanistra-studio-17-service-station.jpg" -OutFile "C:\docs\res\kanistra-studio-17-service-station.jpg"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/AKSizov/winstrap/refs/heads/master/res/disable_lwin.ahk" -OutFile "C:\docs\res\disable_lwin.ahk"
+# Invoke-WebRequest -Uri "https://raw.githubusercontent.com/AKSizov/winstrap/refs/heads/master/res/disable_lwin.ahk" -OutFile "C:\docs\res\disable_lwin.ahk"
 explorer C:\docs\res
